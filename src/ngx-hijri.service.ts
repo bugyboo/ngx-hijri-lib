61c7add74be05da58a6eaf49c496f061c3069187
Yasser Al Harbi <yasser.harbi@gmail.com>
2017-10-25 23:47:38 +0300

    ...

--- src/ngx-hijri.service.ts
+++ src/ngx-hijri.service.ts
@@ -2,11 +2,17 @@ import { Injectable } from '@angular/core';
 import * as Xsoh from 'hijri-js';
 
 @Injectable()
-export class HijriService {
+export class HijriService implements hijriJs.HijriDate {
+
+    public year: string;
+    public month: string;
+    public day: string;
+    public splitter: string;
+    public full: string;
 
     public getResult(): string {
         const x = new Xsoh.default();
-        const h: Xsoh.HijriDate = x.today();
+        const h: hijriJs.HijriDate = x.today();
         return 'It works!! = today is = ' + h.full
         + ' GtoH = ' + x.toHijri('12122000', '').full
         + ' HtoG = ' + x.toGregorian('11-7-1386', '-');
