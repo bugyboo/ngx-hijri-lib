1de400f8784d2bc010ffe9cb80d3255f199ad5e2
Yasser Al Harbi <yasser.harbi@gmail.com>
2017-10-27 02:23:22 +0300

    fixed hijri-js

--- src/ngx-hijri.service.ts
+++ src/ngx-hijri.service.ts
@@ -1,26 +1,33 @@
 import { Injectable } from '@angular/core';
-import { HijriDate, HijriJs, Lang, LangFormat, default as Xsoh } from 'hijri-js';
-// import * as Xsoh from 'hijri-js';
+import * as Xsoh from 'hijri-js';
 
 @Injectable()
 export class HijriService {
 
     public getResult(): string {
-        const x = new Xsoh();
-        console.log(' ==> ', x);
-        const h: HijriDate = x.today();
 
-        console.log(' Foo ==> ', this.getFoo());
+        const x = Xsoh.intilizeHijriJs();
 
+        const defLang: Xsoh.Lang = {
+           prefix: 'ar',
+           notation: 'Hijri',
+           monthNames: ['صفر', 'محرم', 'ربيع 1', 'ربيع 2',
+           'جماد 1', 'جماد 2', 'رجب', 'شعبان', 'رمضان', 'شوال', 'القعده', 'الحجه'],
+           monthShortNames: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'],
+        } as Xsoh.Lang;
+
+        x.setLang(defLang);
+        const h: Xsoh.HijriDate = x.today();
         return 'It works!! = today is = ' + h.full
-        + ' GtoH = ' + x.toHijri('12122000', '').full
+        + ' GtoH = ' + x.toHijri('12122000', '').plain
         + ' HtoG = ' + x.toGregorian('11-7-1386', '-');
     }
-
+    /*
     public getFoo(): Lang {
         const x: Lang = new LangFormat();
         x.prefix = 'Ar';
         x.notation = 'B.c';
         return x;
     }
+    */
 }
